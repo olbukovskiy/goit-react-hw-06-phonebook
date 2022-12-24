55b4be6a7aa40de65ce576075e323530829e1007
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-12-25 01:10:51 +0200

    add store and slices

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const initialValues = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+export const contactsSlice = createSlice({
+  name: 'contactsSlice',
+  initialState: initialValues,
+  reducers: {
+    addNewContact: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
+
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(),
+            number,
+            name,
+          },
+        };
+      },
+    },
+
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const { addNewContact, deleteContact } = contactsSlice.actions;
