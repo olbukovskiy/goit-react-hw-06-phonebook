0f420d9d193c3643c267d46cff3ab60de28a21fb
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-12-25 21:48:31 +0200

    delete comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,3 @@
-// import { useState, useEffect } from 'react';
-
 import ContactList from '../ContactList';
 import AddContact from '../AddContact';
 import Filter from '../Filter';
@@ -7,17 +5,6 @@ import Filter from '../Filter';
 import css from './App.module.css';
 
 export function App() {
-  // const [contacts, setContacts] = useState(
-  //   () => [...JSON.parse(localStorage.getItem('contacts'))] ?? initialValues
-  // );
-
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   const preparedContacts = JSON.stringify(contacts);
-  //   localStorage.setItem('contacts', preparedContacts);
-  // }, [contacts]);
-
   return (
     <div>
       <p className={css.sectionHeading}>Phonebook</p>
